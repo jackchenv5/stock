c58aa6bdad2db7452a8c960093497871d1e223b5
新用户 <user@macdeMacBook-Air.local>
2025-01-08 23:40:13 +0800

    新增抢红包退出机制

--- autojs/redPaket.js
+++ autojs/redPaket.js
@@ -1,23 +1,28 @@
+toast("抢红包开始....")
+sleep(2000)
 while (true) {
-    home();
-    sleep(1000);
-    click(1150, 2380);
-    sleep(1000);
+    sleep(200);
     var curPkgName = currentPackage();
+    if (curPkgName == "com.android.systemui" || curPkgName == "com.vivo.fingerprintui") {
+      console.log('in config ui ...');
+      break
+    }
+    home(); 
+    click(1150, 2380);
+    sleep(500);
+    curPkgName = currentPackage();
     console.log(curPkgName);
-    if (curPkgName != "com.vivo.hiboard") {
+    if (curPkgName != "com.vivo.hiboard" && curPkgName != "com.bbk.launcher2") {
       console.log("start wechat packet....");
-      sleep(1000);
-      click(550, 2450); //点击红包
-      sleep(1000);
-      click(630, 1780); //点击红包
       sleep(500);
-      back();
-      sleep(200);
-      back();
+      click(550, 2450); //点击红包
+      sleep(500);
+      click(630, 1780); //点击红包
+      home();
       continue;
+      sleep(1000);
     }
     console.log("wait....");
-    sleep(3000);
+    sleep(200);
   }
-  
+toast("抢红包结束....")
